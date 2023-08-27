530574b0dcdd4dd16dddef38585c353495e438e3
Akemi Herrera <akselherrera18@gmail.com>
2023-08-27 17:41:48 -0600

    Uso de useEffect

--- Basics/basic-app/src/Components/Welcome.js
+++ Basics/basic-app/src/Components/Welcome.js
@@ -1,13 +1,24 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 export default function Welcome(props) {
     const [counter, setCounter] = useState(0);
+    const [traffictLight, setTrafficLight] = useState(false);
     const {message, name}  = props;
+
+    useEffect(() => {
+        console.log("Status", traffictLight);
+    }, [traffictLight]);
+
+    const count = () => {
+        setCounter(counter + 1);
+        setTrafficLight(!traffictLight);
+    };
     return (
     <div>
         <h1>Hola, {name}</h1>
         <h2>Contador de React con Hooks</h2>
         <p>El numero del contador es: {counter}</p>
-        <button type='submit' onClick={() => setCounter(counter + 1)}>Sumar contador</button>
+        <p>El semaforo se encuentra: {traffictLight  ? 'Green' : 'Red'}</p>
+        <button type='submit' onClick={count}>Sumar contador</button>
     </div>
     )
 }
